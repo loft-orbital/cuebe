0a120dd5a5569cc6313d67dad9011aa669f90f17
banst <bastyen.a@gmail.com>
2021-10-29 15:10:28 +0200

    refactor!: switch to ssa

--- internal/kubernetes/config.go
+++ internal/kubernetes/config.go
@@ -18,16 +18,13 @@ package kubernetes
 import (
 	"fmt"
 
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/cli-runtime/pkg/resource"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-// DefaultConfigClient returns the kubernetes config and client from default configuration.
+// DefaultConfig returns the kubernetes config and client from default configuration.
 // The default context is used if context is empty.
-func DefaultConfigClient(context string) (*rest.Config, kubernetes.Interface, error) {
+func DefaultConfig(context string) (*rest.Config, error) {
 	rules := clientcmd.NewDefaultClientConfigLoadingRules()
 	rules.DefaultClientConfig = &clientcmd.DefaultClientConfig
 
@@ -40,23 +37,7 @@ func DefaultConfigClient(context string) (*rest.Config, kubernetes.Interface, er
 
 	config, err := ccfg.ClientConfig()
 	if err != nil {
-		return nil, nil, fmt.Errorf("could not get Kubernetes config for context %q: %w", context, err)
+		return nil, fmt.Errorf("could not get Kubernetes config for context %q: %w", context, err)
 	}
-	client, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return nil, nil, fmt.Errorf("could not get Kubernetes client: %w", err)
-	}
-	return config, client, nil
-}
-
-func newRestClient(restConfig rest.Config, gv schema.GroupVersion) (rest.Interface, error) {
-	restConfig.ContentConfig = resource.UnstructuredPlusDefaultContentConfig()
-	restConfig.GroupVersion = &gv
-	if len(gv.Group) == 0 {
-		restConfig.APIPath = "/api"
-	} else {
-		restConfig.APIPath = "/apis"
-	}
-
-	return rest.RESTClientFor(&restConfig)
+	return config, nil
 }
--- internal/kubernetes/deploy.go
+++ internal/kubernetes/deploy.go
@@ -16,16 +16,20 @@ limitations under the License.
 package kubernetes
 
 import (
+	"container/list"
+	"context"
+	"encoding/json"
 	"fmt"
 	"strings"
 
 	"github.com/banzaicloud/k8s-objectmatcher/patch"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/cli-runtime/pkg/resource"
-	"k8s.io/client-go/kubernetes"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/discovery/cached/memory"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/restmapper"
 )
@@ -39,106 +43,86 @@ var (
 	patchMaker   = patch.NewPatchMaker(annotator)
 )
 
-// Deploy deploys obj to the given cluster.
-// If the obj already exist in the cluster, a three way merge patch will be computed.
-// If the patch is not empty, the current object will be replaced by obj.
-// TODO support dry-run
-// TODO manage obj validation
-func Deploy(client kubernetes.Interface, restConfig rest.Config, obj runtime.Object) error {
-	groupResources, err := restmapper.GetAPIGroupResources(client.Discovery())
-	if err != nil {
-		return err
-	}
-	rm := restmapper.NewDiscoveryRESTMapper(groupResources)
-
-	// Get some metadata needed to make the REST request.
-	gvk := obj.GetObjectKind().GroupVersionKind()
-	mapping, err := rm.RESTMapping(schema.GroupKind{Group: gvk.Group, Kind: gvk.Kind}, gvk.Version)
+// PatchObjects patches a list of *unstructured.Unstructured object
+func PatchObjects(ctx context.Context, cfg *rest.Config, objs *list.List) error {
+	// Get dynamic client
+	dync, err := dynamic.NewForConfig(cfg)
 	if err != nil {
 		return err
 	}
 
-	// Create a client specifically for creating the object.
-	restClient, err := newRestClient(restConfig, mapping.GroupVersionKind.GroupVersion())
+	// Get discovery client
+	disc, err := discovery.NewDiscoveryClientForConfig(cfg)
 	if err != nil {
 		return err
 	}
+	// Create the discovery cache
+	dcache := memory.NewMemCacheClient(disc)
+	// Get rest mapper
+	rm := restmapper.NewDeferredDiscoveryRESTMapper(dcache)
 
-	// Get useful object meta
-	helper := resource.NewHelper(restClient, mapping)
-	name, err := metaAccessor.Name(obj)
-	if err != nil {
-		return err
-	}
-	ns, err := metaAccessor.Namespace(obj)
-	if err != nil {
-		return err
-	}
-	if ns == "" && helper.NamespaceScoped {
-		ns = defaultNamespace // Set default namespace for namespaced resource with no namespace
-	}
+	// Start patching
+	for initlen := objs.Len(); initlen > 0; initlen = objs.Len() {
+		dcache.Invalidate() // Invalidate the cache to get new resource mappings
 
-	current, err := helper.Get(ns, name)
-	if err != nil {
-		if !apierrors.IsNotFound(err) {
-			return err
+		for e := objs.Front(); e != nil; e = e.Next() {
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			default:
+				// Retrieve object
+				obj, ok := e.Value.(*unstructured.Unstructured)
+				if !ok {
+					return fmt.Errorf("Unexpected type for %v, should be *unstructured.Unstructured", e.Value)
+				}
+
+				// Get mapping
+				gvk := obj.GroupVersionKind()
+				mapping, err := rm.RESTMapping(gvk.GroupKind(), gvk.Version)
+				if err != nil {
+					// couldn't find mapping, try next loop
+					continue
+				}
+				var dr dynamic.ResourceInterface
+				if mapping.Scope.Name() == meta.RESTScopeNameNamespace {
+					// Namespaced resources
+					ns := obj.GetNamespace()
+					if ns == "" {
+						ns = "default"
+					}
+					dr = dync.Resource(mapping.Resource).Namespace(ns)
+				} else {
+					// Cluster-wide resources
+					dr = dync.Resource(mapping.Resource)
+				}
+				data, err := json.Marshal(obj)
+				if err != nil {
+					return fmt.Errorf("Couldn't marshal %s: %w", printObj(obj), err)
+				}
+
+				// TODO maybe retrieve from option, to enable dryrun
+				po := metav1.PatchOptions{
+					FieldManager: "cuebe",
+				}
+				// Patch object
+				if _, err := dr.Patch(ctx, obj.GetName(), types.ApplyPatchType, data, po); err != nil {
+					return fmt.Errorf("Couldn't patch %s: %w", printObj(obj), err)
+				}
+				fmt.Printf("%s patched\n", printObj(obj))
+				// Object successfully patched, remove from queue
+				objs.Remove(e)
+			}
 		}
-		// Object does not exist yet
-		return create(obj, ns, helper)
-	}
-	// Object already exist, check if we need to update it
-	patchResult, err := patchMaker.Calculate(current, obj, patch.IgnoreStatusFields())
-	if err != nil {
-		return err
+
+		// Found a deadlock
+		if objs.Len() >= initlen {
+			return fmt.Errorf("Failed to patch every objects, %d remaining", objs.Len())
+		}
 	}
 
-	if patchResult.IsEmpty() {
-		// Nothing to do
-		fmt.Println(buildFeedback(gvk.Kind, name, "unchanged", helper.ServerDryRun))
-		return nil
-	}
-
-	// Update the object
-	return update(obj, ns, name, helper)
-}
-
-func update(obj runtime.Object, ns, name string, helper *resource.Helper) error {
-	if err := annotator.SetLastAppliedAnnotation(obj); err != nil { // Add last configuration annotation
-		return err
-	}
-	nobj, err := helper.Replace(ns, name, true, obj)
-	if err != nil {
-		return err
-	}
-	gvk := nobj.GetObjectKind().GroupVersionKind()
-	if err != nil {
-		return err
-	}
-	fmt.Println(buildFeedback(gvk.Kind, name, "updated", helper.ServerDryRun))
 	return nil
 }
 
-func create(obj runtime.Object, ns string, helper *resource.Helper) error {
-	if err := annotator.SetLastAppliedAnnotation(obj); err != nil { // Add last configuration annotation
-		return err
-	}
-	nobj, err := helper.Create(ns, true, obj) // Create the object
-	if err != nil {
-		return err
-	}
-	gvk := nobj.GetObjectKind().GroupVersionKind()
-	name, err := metaAccessor.Name(obj)
-	if err != nil {
-		return err
-	}
-	fmt.Println(buildFeedback(gvk.Kind, name, "created", helper.ServerDryRun))
-	return nil
-}
-
-func buildFeedback(kind, name, verb string, dryrun bool) string {
-	s := fmt.Sprintf("%s/%s %s", kind, name, verb)
-	if dryrun {
-		s += " (server dry run)"
-	}
-	return strings.ToLower(s)
+func printObj(obj *unstructured.Unstructured) string {
+	return strings.ToLower(fmt.Sprintf("%s/%s", obj.GetKind(), obj.GetName()))
 }
