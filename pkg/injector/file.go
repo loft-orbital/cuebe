914faf82fcb65d8cd4ddbb5091302d45f3202cc6
bastien <bastien@loftorbital.com>
2021-12-14 19:06:56 +0000

    feat: add file injection

--- pkg/injector/file.go
+++ pkg/injector/file.go
@@ -1,6 +1,15 @@
 package injector
 
-import "cuelang.org/go/cue"
+import (
+	"encoding/json"
+	"fmt"
+	"path"
+
+	"cuelang.org/go/cue"
+	"github.com/PaesslerAG/jsonpath"
+	"github.com/loft-orbital/cuebe/pkg/unifier"
+	"gopkg.in/yaml.v2"
+)
 
 type File struct {
 	path   cue.Path
@@ -8,12 +17,47 @@ type File struct {
 }
 
 func NewFile(src, srcPath, dstPath string) *File {
-	r := make(chan interface{})
-	// TODO fire gorotine to parse file
+	r := make(chan interface{}, 1)
+	go parseFile(src, srcPath, r)
 	return &File{path: cue.ParsePath(dstPath), result: r}
 }
 
 func (f *File) Inject(target cue.Value) cue.Value {
 	r := <-f.result
 	return target.FillPath(f.path, r)
+}
+
+func parseFile(file, jpath string, res chan<- interface{}) {
+	defer close(res)
+
+	// read
+	b, err := unifier.ReadFile(file)
+	if err != nil {
+		res <- err
+		return
+	}
+
+	// unmarshal
+	var v interface{}
+	switch path.Ext(file) {
+	case ".json":
+		err = json.Unmarshal(b, &v)
+	case ".yaml", ".yml":
+		err = yaml.Unmarshal(b, &v)
+	default:
+		err = fmt.Errorf("Unsupported extension %s", path.Ext(file))
+	}
+	if err != nil {
+		res <- fmt.Errorf("failed to unmarshal: %w", err)
+		return
+	}
+
+	// get path
+	v, err = jsonpath.Get(jpath, v)
+	if err != nil {
+		res <- fmt.Errorf("failed to extract path: %w", err)
+		return
+	}
+
+	res <- v
 }
---
+++ pkg/injector/file_test.go
@@ -0,0 +1,52 @@
+package injector
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"cuelang.org/go/cue/cuecontext"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type Spacecraft struct {
+	Name     string `json:"name"`
+	Power    int    `json:"power"`
+	Revision int    `json:"revision"`
+}
+
+func TestParseFile(t *testing.T) {
+	f, err := ioutil.TempFile("", "test_inject*.json")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+	defer f.Close()
+
+	b, _ := json.Marshal(Spacecraft{"Voyager", 470, 1})
+	f.Write(b)
+	res := make(chan interface{})
+	go parseFile(f.Name(), "$.power", res)
+
+	r := <-res
+	assert.Equal(t, float64(470), r)
+}
+
+func TestInject(t *testing.T) {
+	f, err := ioutil.TempFile("", "test_inject*.json")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+	defer f.Close()
+
+	ctx := cuecontext.New()
+	v := ctx.CompileString("spacecraft: name: string")
+
+	b, _ := json.Marshal(Spacecraft{"Voyager", 470, 1})
+	f.Write(b)
+	fi := NewFile(f.Name(), "$.name", "spacecraft.name")
+	v = fi.Inject(v)
+
+	actual, err := v.MarshalJSON()
+	assert.NoError(t, err)
+	assert.Equal(t, `{"spacecraft":{"name":"Voyager"}}`, string(actual))
+}
