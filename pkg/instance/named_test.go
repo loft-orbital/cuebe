9896ddaf105f12dc155da3e02d07d4a0d9838a3b
banst <bastyen.a@gmail.com>
2022-04-12 23:10:04 +0200

    feat: implements instance concept

---
+++ pkg/instance/named_test.go
@@ -0,0 +1,206 @@
+package instance
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/loft-orbital/cuebe/internal/mock"
+	"github.com/loft-orbital/cuebe/internal/utils"
+	"github.com/loft-orbital/cuebe/pkg/manifest"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+func newUniqueManifest() manifest.Manifest {
+	m := manifest.New(new(unstructured.Unstructured))
+	name := make([]byte, 16)
+	rand.Read(name)
+	m.SetName(fmt.Sprintf("%s-%d", base64.StdEncoding.EncodeToString(name), time.Now().UnixNano()))
+
+	return m
+}
+
+func TestNewNamed(t *testing.T) {
+	i := NewNamed("potato")
+	assert.IsType(t, (*Named)(nil), i)
+	assert.Implements(t, (*Instance)(nil), i)
+}
+
+func TestNamedManifests(t *testing.T) {
+	i := &Named{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "potato",
+		},
+		manifests: map[manifest.Id]manifest.Manifest{
+			{Name: "one"}:   newUniqueManifest(),
+			{Name: "two"}:   newUniqueManifest(),
+			{Name: "three"}: newUniqueManifest(),
+		},
+	}
+	assert.Len(t, i.Manifests(), 3)
+}
+
+func TestNamedAdd(t *testing.T) {
+	instance := NewNamed("potato")
+	var wg sync.WaitGroup
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			instance.Add(newUniqueManifest())
+		}()
+	}
+	wg.Wait()
+
+	assert.Len(t, instance.Manifests(), 100)
+}
+
+func TestNamedRemove(t *testing.T) {
+	mfs := []manifest.Manifest{
+		newUniqueManifest(),
+		newUniqueManifest(),
+		newUniqueManifest(),
+	}
+
+	instance := &Named{manifests: map[manifest.Id]manifest.Manifest{
+		mfs[0].Id(): mfs[0],
+		// we don't add 1 on purpose to test deletion of a manifest that does not exist
+		mfs[2].Id(): mfs[2],
+	}}
+	var wg sync.WaitGroup
+
+	for i := 0; i < len(mfs)-1; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			instance.Remove(mfs[i])
+		}(i)
+	}
+	wg.Wait()
+
+	assert.Len(t, instance.Manifests(), 1)
+}
+
+func TestNamedMarshal(t *testing.T) {
+	instance := NewNamed("potato")
+	b, err := instance.Marshal()
+	assert.NoError(t, err)
+	expected := `
+{
+   "apiVersion":"cuebe.loftorbital.com/v1alpha1",
+   "kind":"Instance",
+   "metadata":{
+      "creationTimestamp":null,
+      "name":"potato"
+   },
+   "spec":{
+
+   }
+}
+`
+	assert.JSONEq(t, expected, string(b))
+}
+
+func TestNamedUnmarshal(t *testing.T) {
+	m := newUniqueManifest()
+	u := new(unstructured.Unstructured)
+	u.SetName("tomato")
+	u.SetUnstructuredContent(map[string]interface{}{
+		"spec": InstanceSpec{Resources: []manifest.Id{m.Id()}},
+	})
+	data, err := u.MarshalJSON()
+	require.NoError(t, err)
+
+	ni := NewNamed("potato")
+	assert.NoError(t, ni.Unmarshal(data))
+	assert.Equal(t, m.Id(), ni.Spec.Resources[0])
+}
+
+func TestNamedDelete(t *testing.T) {
+	konfig, tfake, client := utils.NewFakeK8sConfig()
+	coreresources := &metav1.APIResourceList{
+		GroupVersion: Group + "/" + Version,
+		APIResources: []metav1.APIResource{
+			{
+				Name:       Resource,
+				Kind:       Kind,
+				Verbs:      metav1.Verbs{"delete"},
+				Namespaced: false,
+			},
+		},
+	}
+	tfake.Resources = append(tfake.Resources, coreresources)
+
+	m := manifest.New(new(unstructured.Unstructured))
+	ni := NewNamed("potato")
+	ni.Add(m)
+	cluster := mock.NewCluster(client, tfake.Resources...)
+	rid := ni.Id()
+	cluster.Resources.Store(rid, ni)
+
+	assert.NoError(t, ni.Delete(context.Background(), konfig, utils.CommonMetaOptions{}))
+	assert.Len(t, ni.Manifests(), 0)
+	assert.Len(t, ni.Spec.Resources, 0)
+	assert.Falsef(t, cluster.Contains(rid), "Cluster should not contain the instance after delete")
+}
+
+func TestNamedCommit(t *testing.T) {
+	// prepare cluster
+	konfig, tfake, client := utils.NewFakeK8sConfig()
+	coreresources := &metav1.APIResourceList{
+		GroupVersion: "v1",
+		APIResources: []metav1.APIResource{
+			{
+				Name:       "configmaps",
+				Kind:       "ConfigMap",
+				Verbs:      metav1.Verbs{"delete, create, patch, get"},
+				Namespaced: true,
+			},
+		},
+	}
+	instresources := &metav1.APIResourceList{
+		GroupVersion: Group + "/" + Version,
+		APIResources: []metav1.APIResource{
+			{
+				Name:       Resource,
+				Kind:       Kind,
+				Verbs:      metav1.Verbs{"delete, create, patch, get"},
+				Namespaced: false,
+			},
+		},
+	}
+	tfake.Resources = append(tfake.Resources, coreresources, instresources)
+	cluster := mock.NewCluster(client, tfake.Resources...)
+
+	// prepare instance
+	ni := NewNamed("potato")
+	m := newUniqueManifest()
+	m.SetKind("ConfigMap")
+	m.SetAPIVersion("v1")
+	m.SetNamespace("default")
+	ni.Add(m)
+	mprune := newUniqueManifest()
+	mprune.SetKind("ConfigMap")
+	mprune.SetAPIVersion("v1")
+	mprune.SetNamespace("default")
+	ni.Spec.Resources = []manifest.Id{mprune.Id()}
+	cluster.Resources.Store(mprune.Id(), mprune)
+
+	// commit
+	assert.NoError(t, ni.Commit(context.Background(), konfig, utils.CommonMetaOptions{}))
+
+	// verify
+	assert.True(t, cluster.Contains(ni.Id()), "Cluster should contain the instance after commit")
+	assert.True(t, cluster.Contains(m.Id()), "Cluster should contain the manifest after commit")
+	assert.False(t, cluster.Contains(mprune.Id()), "Cluster should contain the pruned manifest after commit")
+	assert.Len(t, ni.Spec.Resources, 1)
+	assert.Equal(t, m.Id(), ni.Spec.Resources[0])
+}
---
+++ pkg/instance/orphan.go
@@ -0,0 +1,105 @@
+package instance
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"github.com/loft-orbital/cuebe/internal/utils"
+	"github.com/loft-orbital/cuebe/pkg/manifest"
+)
+
+type Orphan struct {
+	manifests map[manifest.Id]manifest.Manifest
+	guard     sync.Mutex
+}
+
+// NewOrphan creates a new Orphan instance.
+func NewOrphan() *Orphan {
+	return new(Orphan)
+}
+
+// Commit applies the instance remotely.
+func (o *Orphan) Commit(ctx context.Context, config *utils.K8sConfig, opts utils.CommonMetaOptions) error {
+	mfs := o.Manifests()
+
+	cerr := make(chan error, len(mfs))
+	var wg sync.WaitGroup
+	for _, m := range mfs {
+		wg.Add(1)
+		go func(m manifest.Manifest) {
+			defer wg.Done()
+			newM, err := m.Patch(ctx, config.RESTMapper, config.DynamicClient, opts.PatchOptions())
+			if err != nil {
+				cerr <- fmt.Errorf("applying manifest %s: %w", m.Id(), err)
+				return
+			}
+			o.Add(newM)
+		}(m)
+	}
+	wg.Wait()
+	close(cerr)
+
+	return utils.CollectErrors(cerr)
+}
+
+// Delete deletes the instance from the cluster.
+func (o *Orphan) Delete(ctx context.Context, config *utils.K8sConfig, opts utils.CommonMetaOptions) error {
+	mfs := o.Manifests()
+
+	cerr := make(chan error, len(mfs))
+	var wg sync.WaitGroup
+	for _, m := range mfs {
+		wg.Add(1)
+		go func(m manifest.Manifest) {
+			defer wg.Done()
+			err := m.Delete(ctx, config.RESTMapper, config.DynamicClient, opts.DeleteOptions())
+			if err != nil {
+				cerr <- fmt.Errorf("deleting manifest %s: %w", m.Id(), err)
+				return
+			}
+			o.Remove(m)
+		}(m)
+	}
+	wg.Wait()
+	close(cerr)
+
+	return utils.CollectErrors(cerr)
+}
+
+// Add adds a Manifest to the instance.
+//
+// It does not commit changes remotely, use Commit for that.
+func (o *Orphan) Add(m manifest.Manifest) {
+	o.guard.Lock()
+	defer o.guard.Unlock()
+	if o.manifests == nil {
+		o.manifests = make(map[manifest.Id]manifest.Manifest, 1)
+	}
+	o.manifests[m.Id()] = m
+}
+
+// Remove removes a Manifest from the instance
+//
+// It does not commit changes remotely, use Commit for that.
+func (o *Orphan) Remove(m manifest.Manifest) {
+	o.guard.Lock()
+	defer o.guard.Unlock()
+	delete(o.manifests, m.Id())
+}
+
+// Manifests returns the instance manifests.
+func (o *Orphan) Manifests() []manifest.Manifest {
+	o.guard.Lock()
+	defer o.guard.Unlock()
+
+	mfs := make([]manifest.Manifest, 0, len(o.manifests))
+	for _, m := range o.manifests {
+		mfs = append(mfs, m)
+	}
+	return mfs
+}
+
+func (o *Orphan) String() string {
+	return "orphan"
+}
