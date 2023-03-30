dc0b7656f0e20cf8451c36f31c1551c42180a406
Thomas MICHEL <thomas.michel@loftorbital.com>
2023-03-30 12:45:33 +0200

    chore: fmting

--- cmd/cuebe/cmd/eval.go
+++ cmd/cuebe/cmd/eval.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,12 +16,12 @@ limitations under the License.
 package cmd
 
 import (
-    "github.com/loft-orbital/cuebe/cmd/cuebe/factory"
-	"github.com/spf13/cobra"
-	"fmt"
 	"cuelang.org/go/cue"
-	"github.com/loft-orbital/cuebe/pkg/build"
 	"cuelang.org/go/cue/load"
+	"fmt"
+	"github.com/loft-orbital/cuebe/cmd/cuebe/factory"
+	"github.com/loft-orbital/cuebe/pkg/build"
+	"github.com/spf13/cobra"
 )
 
 func newEvalCmd() *cobra.Command {
@@ -47,36 +47,36 @@ cuebe eval .
 }
 
 func runEval(cmd *cobra.Command, args []string) {
-    // Build opts, shall return a BuildOpt{} struct
-    opts := factory.GetBuildOpt(cmd)
+	// Build opts, shall return a BuildOpt{} struct
+	opts := factory.GetBuildOpt(cmd)
 
-    // build the cuebe context, and return a cue.Value with the unified cue structure
-    v, err := build.Build(factory.GetBuildContext(cmd), &load.Config{
-        Tags:    opts.Tags,
-        TagVars: load.DefaultTagVars(),
-    })
-    if err != nil {
-        fmt.Errorf("could not build context: %w", err)
-    }
+	// build the cuebe context, and return a cue.Value with the unified cue structure
+	v, err := build.Build(factory.GetBuildContext(cmd), &load.Config{
+		Tags:    opts.Tags,
+		TagVars: load.DefaultTagVars(),
+	})
+	if err != nil {
+		fmt.Errorf("could not build context: %w", err)
+	}
 
-    // Parse paths expressions (-e argument)
-    paths := make([]cue.Path, 0, len(opts.Expressions))
-    for _, e := range opts.Expressions {
-        p := cue.ParsePath(e)
-        if p.Err() != nil {
-            fmt.Errorf("failed to parse expression %s: %w", e, p.Err())
-        }
-        paths = append(paths, p)
-    }
+	// Parse paths expressions (-e argument)
+	paths := make([]cue.Path, 0, len(opts.Expressions))
+	for _, e := range opts.Expressions {
+		p := cue.ParsePath(e)
+		if p.Err() != nil {
+			fmt.Errorf("failed to parse expression %s: %w", e, p.Err())
+		}
+		paths = append(paths, p)
+	}
 
-    // Output the cue evaluation WITHOUT concrete values
-    // If we have no paths, we dump the whole unified values
-    if len(paths) == 0 {
-        fmt.Printf("%v", v)
-    } else {
-        for _, p := range paths {
-            node := v.LookupPath(p)
-            fmt.Printf("%v", node)
-        }
-    }
+	// Output the cue evaluation WITHOUT concrete values
+	// If we have no paths, we dump the whole unified values
+	if len(paths) == 0 {
+		fmt.Printf("%v", v)
+	} else {
+		for _, p := range paths {
+			node := v.LookupPath(p)
+			fmt.Printf("%v", node)
+		}
+	}
 }
